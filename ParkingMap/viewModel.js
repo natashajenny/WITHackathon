acf0039ae4dcd9a5a3688cddaaccf477f3c5c295
RFSurdsman <ryan_fallah@hotmail.com>
2018-10-13 14:04:06 +1100

    KO

--- ParkingMap/viewModel.js
+++ ParkingMap/viewModel.js
@@ -3,6 +3,26 @@ function ViewModel() {
 	var self = this;
 
 	self.floors = ko.observableArray(allFloors)
+
+    self.items = ko.observableArray(
+		[{"Name": 'Ryan', "Id": 'flash'},
+		{"Name": 'Ryan', "Id": 'flash'},
+		{"Name": 'Ryan', "Id": 'flash'},
+		{"Name": 'Ryan', "Id": 'flash'},
+		{"Name": 'Ryan', "Id": 'flash'}]
+	);
+    self.items_grouped = ko.computed(function(){
+        var grouped = [];
+        var itemlist = self.items();
+        for(var i = 0; i < itemlist.length; i+=5){
+            var row = [];
+            for(var j = 0; j < 5; ++j){
+                row.push(itemlist[i+j]);
+			}
+			grouped.push(row);
+        }
+        return grouped;
+    });
 }
 
 ko.applyBindings(new ViewModel())
